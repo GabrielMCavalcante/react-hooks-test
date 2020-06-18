4cba7a552b7304f9104c125b41650260a7d2a69e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-18 13:03:17 -0300

    Added Auth component

---
+++ src/components/Auth.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+
+import Card from './UI/Card'
+import './Auth.css'
+import { AuthContext } from '../context/auth-context'
+
+const Auth = () => {
+
+  const authContext = useContext(AuthContext)
+
+  return (
+    <div className="auth">
+      <Card>
+        <h2>You are not authenticated!</h2>
+        <p>Please log in to continue.</p>
+        <button onClick={authContext.login}>Log In</button>
+      </Card>
+    </div>
+  )
+}
+
+export default Auth
